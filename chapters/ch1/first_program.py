ee4907974e1a3c311e84b3d5814794a28e04637e
ajayi-ei <ajayiisaiah777@gmail.com>
2023-04-10 11:19:47 +0700

    Challenge 1

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,8 @@
+# 1.Ask the user for their first name
+first_name = input('Please enter your first name: ')
+
+# 2.Ask the user for their last name
+last_name = input('Please enter your last name ')
+
+# 3.Print their full name
+print(f'Your full name is {first_name} {last_name}')
