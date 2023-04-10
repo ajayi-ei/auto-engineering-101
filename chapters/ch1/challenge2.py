2affd5c725c5613f333df7460fa22bf01514e163
ajayi-ei <ajayiisaiah777@gmail.com>
2023-04-10 11:34:35 +0700

    Challenge 2

---
+++ chapters/ch1/challenge2.py
@@ -0,0 +1,10 @@
+# Ask the user for a season of the year
+season = input('Please enter a season of the year: ')
+
+# Ask the user for a random number
+number = input('please enter a random number: ')
+
+# Ask the user for an adjective
+adjective = input('Please enter an adjective: ')
+
+print(f'On a {adjective} {season} day, I drink at least {number} cups of juice!')
